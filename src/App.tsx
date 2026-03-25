6f4f430fb70a50e8ae0e391341140807a4336582
Gabstaudt <macstaudt@gmail.com>
2026-03-25 11:44:44 -0300

    feat: mudança das cores de ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
+import EmployeePanel from "./pages/EmployeePanel.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/freezer" element={<EmployeePanel />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
